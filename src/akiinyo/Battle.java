0843f6a2a9b183ae7b4ba2cc0fb1b0ec54227547
Tagaki Aki <a-tagaki@esm.co.jp>
2011-08-16 10:31:05 +0900

    途中経過

---
+++ src/akiinyo/Battle.java
@@ -0,0 +1,112 @@
+package akiinyo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Battle {
+	private String player = "あき";
+	private int myHP = 10;
+	private int myMP = 6;
+	private String monster = "スライム";
+	private int monsterHP = 8;
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		Battle battle = new Battle();
+		battle.doit();
+	}
+
+	public void doit() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("----------------");
+		System.out.println(monster + "があらわれた!");
+		System.out.println("----------------");
+		for (;;) {
+			if (myHP < 1 || monsterHP < 1) {
+				break;
+			}
+			myturn(in);
+			String s1 = in.readLine();
+			if (s1.length() <= 0 && monsterHP > 1) {
+				MonsterTurn();
+			}
+			String s2 = in.readLine();
+			if (s2.length() <= 0) {
+				continue;
+			}
+		}
+		if (monsterHP < 1) {
+			System.out.println("----------------");
+			System.out.println(monster + "をたおした！");
+			System.out.println("----------------");
+		} else if (myHP < 1) {
+			System.out.println("----------------");
+			System.out.println("ゲームオーバー");
+			System.out.println("----------------");
+		}
+	}
+
+	public void myturn(BufferedReader in) throws IOException {
+		System.out.println("HP = " + myHP + "  " + "MP = " + myMP);
+		System.out.println("コマンド");
+		System.out.println("1.たたかう");
+		System.out.println("2.ホイミ");
+		// System.out.println("3.にげる");
+		String command = in.readLine();
+		if (command.equals("1")) {
+			attack();
+		} else if (command.equals("2")) {
+			recovery();
+			// } else if (command.equals("3")) {
+			// escape();
+		}
+	}
+
+	public void attack() {
+		Random point = new Random();
+		int power = point.nextInt(3) + 1;
+		System.out.println(monster + "に" + power + "のこうげき");
+		monsterHP -= power;
+	}
+
+	public void recovery() {
+		System.out.println("ホイミをとなえた！");
+		if (myMP < 2) {
+			System.out.println("しかし、MPが足りない");
+		} else if (myHP == 10) {
+			System.out.println("しかし、今の" + player + "には必要なさそうだ");
+		} else if (myHP == 9) {
+			myHP += 1;
+			myMP -= 2;
+			System.out.println("HPが1かいふく");
+			System.out.println("HP = " + myHP + "  " + "MP = " + myMP);
+		} else if (myHP <= 8) {
+			myHP += 2;
+			myMP -= 2;
+			System.out.println("HPが2かいふく");
+			System.out.println("HP = " + myHP + "  " + "MP = " + myMP);
+		}
+	}
+
+	public void MonsterTurn() {
+		System.out.println("----------------");
+		System.out.println(monster + "のこうげき");
+		Random point = new Random();
+		int power = point.nextInt(3) + 1;
+		System.out.println(player + "に" + power + "のダメージ");
+		myHP -= power;
+		System.out.println("----------------");
+	}
+
+	// public void escape() {
+	// System.out.println("----------------");
+	// System.out.println(player + "は逃げ出した");
+	// System.out.println("----------------");
+	// }
+
+}
